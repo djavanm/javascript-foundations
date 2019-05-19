40153affe0b5355b5c613c3d3691308532a72b1b
Djavan Munroe <djavanm24@gmail.com>
2019-05-19 16:37:22 -0600

    Start fairy

--- mythical-creatures/exercises/fairy.js
+++ mythical-creatures/exercises/fairy.js
@@ -1,5 +1,17 @@
 class Fairy {
-  constructor() {
+  constructor(name) {
+    this.name = name;
+    this.dust = 10;
+    this.clothes = {dresses: ['Iris', ]};
+ }
+ receiveBelief() {
+   this.dust += 1;
+ }
+ believe() {
+   this.dust += 10;
+ }
+ makeDresses(array) {
+   this.clothes.dresses.push(array);
  }
 }
 module.exports = Fairy;
