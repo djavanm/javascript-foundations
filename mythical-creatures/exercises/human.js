24b9fe6abc230fd46b9c7288c6dcf217ed3c0f20
Djavan Munroe <djavanm24@gmail.com>
2019-05-20 18:23:27 -0600

    Finish Ogre

--- mythical-creatures/exercises/human.js
+++ mythical-creatures/exercises/human.js
@@ -1,7 +1,20 @@
 class Human {
-  constructor() {
- }
-}
+  constructor(name) {
+    this.name = name;
+    this.encounterCounter = 0;
+    this.knockedOut = false;
+  }
+  noticesOgre() {
+  if(this.encounterCounter == 0) {
+    return false;
+  } 
+  if(this.encounterCounter % 3 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+  }
+  }
 module.exports = Human;
 
 // npm test mythical-creatures/test/human-test.js
--- mythical-creatures/exercises/ogre.js
+++ mythical-creatures/exercises/ogre.js
@@ -1,5 +1,23 @@
 class Ogre {
-  constructor() {
+  constructor(name, home) {
+    this.name = name;
+    this.home = home || 'Swamp';
+    this.swings = 0;
+ }
+ encounter(human) {
+   human.encounterCounter++;
+   if(human.encounterCounter % 3 == 0) {
+     this.swings++;
+   if(this.swings % 2 == 0) {
+      human.knockedOut = true;
+   }
+ }
+}
+swingAt(human) {
+  this.swings++;
+ } 
+ apologize(human){
+   human.knockedOut = false;
  }
 }
 module.exports = Ogre;
