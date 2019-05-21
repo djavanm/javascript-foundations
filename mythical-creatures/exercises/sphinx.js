f884727af5b0c962e3393dccf142e690efb4e76b
Djavan Munroe <djavanm24@gmail.com>
2019-05-20 18:57:56 -0600

    Start sphinx

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -1,6 +1,20 @@
 class Sphinx {
   constructor() {
+    this.riddles = [];
  }
+ collectRiddle(riddle) {
+   this.riddles.push(riddle);
+   if (this.riddles.length > 3) {
+     this.riddles.shift();
+   }
+ }
+attemptAnswer(string){
+for(this.i=0;this.i<this.riddles.length;this.i++) {
+  if(string == this.riddles[this.i].answer) {
+    this.riddles.splice([this.i], 1)
+  }
+}
+}
 }
 module.exports = Sphinx;
 
