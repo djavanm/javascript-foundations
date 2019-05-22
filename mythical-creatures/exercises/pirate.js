7da38654ef6c0aa5bc7750992eac1c42920821b0
Djavan Munroe <djavanm24@gmail.com>
2019-05-21 21:25:01 -0600

    Redo pirate

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -1,26 +1,21 @@
 class Pirate {
-  constructor(name, job, cursed, heinousActs, booty) {
-  	this.name = name;
-    if(job === undefined) {
-  	this.job = 'Scallywag';
-    } else {
-  	this.job = job; 	
-   }
-    this.cursed = false;
-    this.heinousActs = 0;
-    this.booty = 0;
-  }
+  constructor(name, job ) {
+ this.name = name;
+ this.job = job || 'Scallywag';
+ this.cursed = false;
+ this.heinousActs = 0;
+ this.booty = 0;
+ }
   commitHeinousAct() {
-  	this.heinousActs +=1 ;
-  	if(this.heinousActs > 2) {
-  		this.cursed = true;
-  	}
-  }
+  	this.heinousActs++ ;
+    this.heinousActs > 2 ? this.cursed = true : false;
+    }
+
   robShip() {
   	this.booty = 100;
   	return 'YAARRR!';
   }
- }
+}
 module.exports = Pirate;
 
 // npm test mythical-creatures/test/pirate-test.js
